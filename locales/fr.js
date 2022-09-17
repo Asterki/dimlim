3460e1a2bb78469cbd71de26271057e12bf5e328
Asterki <asterki.dev@proton.me>
2022-09-17 12:35:32 -0600

    Language support

--- locales/fr.js
+++ locales/fr.js
@@ -12,13 +12,38 @@ const fr = {
             },
         },
         home: {
-            title: "Page d'accueil",
+            pageTitle: "DIMLIM | Accueil",
+            title: "Accueil",
 
             navbar: {
                 profile: "Profil",
-                settings: "Réglages",
+                settings: "Paramètres",
 
-                logout: "Se déconnecter",
+                logout: "Déconnexion",
+            },
+
+            contacts: "Contacts",
+            blocked: "Bloqué",
+
+            addContact: "Ajouter un contact",
+            noContacts: "Vous n'avez pas encore de contacts, commencez par appuyer sur Ajouter un contact",
+
+            blockedContacts: "Contacts bloqués",
+            noBlockedContacts: "Vous n'avez pas encore de contacts bloqués",
+
+            dialogs: {
+                addContact: {
+                    title: "Ajouter un contact",
+                    label: "Veuillez insérer le nom d'utilisateur de la personne",
+
+                    cancel: "Annuler",
+                    add: "Ajouter",
+
+                    "missing-parameters": "Veuillez remplir toutes les entrées.",
+                    "self-add": "Vous ne pouvez pas vous ajouter comme contact",
+                    "already-on-list": "Cet utilisateur est déjà dans vos contacts.",
+                    "user-not-found": "Il n'y a pas d'utilisateur avec ce nom d'utilisateur",
+                },
             },
         },
     },
@@ -89,14 +114,183 @@ const fr = {
     },
     settings: {
         index: {
-            pageTitle: "DIMLIM | Réglages",
-            title: "DIMLIM Réglages",
+            pageTitle: "DIMLIM | Paramètres",
+            title: "Paramètres DIMLIM",
 
             navbar: {
                 profile: "Profil",
-                settings: "Réglages",
+                settings: "Paramètres",
 
-                logout: "Se déconnecter",
+                logout: "Déconnexion",
+            },
+
+            tabs: {
+                general: "Général",
+                security: "Sécurité",
+                privacy: "Confidentialité",
+                account: "Compte",
+            },
+
+            general: {
+                title: "Informations générales",
+                username: "Nom d'utilisateur:",
+                bio: "Biographie:",
+                email: "Email:",
+                preferredLanguage: "Langue préférée:",
+                contacts: "Contacts:",
+                blockedContacts: "Contacts bloqués:",
+                creation: "Date de création du compte:",
+
+                locales: {
+                    de: "Deutsch",
+                    en: "English",
+                    es: "Español",
+                    fr: "Français",
+                    pr: "Português",
+                },
+
+                dialogs: {
+                    avatar: {
+                        title: "Voulez-vous utiliser cette image comme avatar?",
+                        cancel: "Annuler",
+                        yes: "Oui",
+                    },
+                    email: {
+                        title: "Veuillez insérer votre nouvel e-mail",
+                        subTitle: "Cet e-mail ne sera pas vérifié et vous devrez passer par le processus de re-vérification de votre e-mail",
+
+                        newEmail: "Votre nouvel e-mail",
+                        password: "Votre mot de passe actuel",
+
+                        cancel: "Annuler",
+                        yes: "Oui",
+
+                        missing: "Veuillez remplir toutes les entrées",
+                        invalidEmail: "L'e-mail que vous avez saisi n'est pas valide",
+
+                        "email-already-in-use": "L'e-mail que vous avez saisi est déjà utilisé",
+                        "user-not-found": "Ok mon pote, comment diable fais-tu ça alors, je ne vais même pas m'embêter",
+                        "rate-limit-exceeded": "Vous avez trop essayé de modifier votre adresse e-mail, veuillez réessayer plus tard.",
+                    },
+                    Langue: {
+                        title: "Veuillez sélectionner une langue",
+                        subTitle: "Ceci définira la langue de l'application",
+
+                        cancel: "Annuler",
+                        change: "Changer de langue",
+                    },
+                    bio: {
+                        title: "Veuillez insérer votre nouvelle biographie",
+                        cancel: "Annuler",
+                        change: "Changer la bio",
+                    },
+                },
+            },
+            security: {
+                password: "Mot de passe",
+                tfa: "Authentification à deux facteurs",
+
+                tfaActive: "Actif",
+                tfaNotActive: "Non configuré",
+
+                dialogs: {
+                    password: {
+                        title: "Modifier le mot de passe",
+                        old: "Votre ancien mot de passe",
+                        new: "Votre nouveau mot de passe",
+                        tfa: "Code TFA/Code de sauvegarde",
+
+                        cancel: "Annuler",
+                        change: "Modifier le mot de passe",
+
+                        unauthorized: "Mot de passe invalide.",
+                        "missing-parameters": "Veuillez remplir toutes les entrées.",
+                        "invalid-parameters": "Votre nouveau mot de passe doit comporter plus de 8 caractères et moins de 256 caractères.",
+                        "invalid-tfa-code": "Code TFA invalide, veuillez réessayer.",
+                        "rate-limit-exceeded": "Vous avez trop essayé de changer votre mot de passe, veuillez réessayer plus tard.",
+                    },
+                    setupTfa: {
+                        title: "Authentification à deux facteurs",
+                        subTitle:
+                            "TFA ajoute une couche de sécurité supplémentaire dans laquelle vous avez également besoin d'un appareil pour vous connecter à votre compte.",
+                        deactivateLabel: "Code TFA/Code de sauvegarde",
+
+                        emailVerified: "Vous devez vérifier votre adresse e-mail pour activer TFA",
+
+                        activate: "Activer",
+                        deactivate: "Désactiver",
+                        cancel: "Annuler",
+                        ok: "OK",
+
+                        "missing-parameters": "Veuillez remplir toutes les entrées.",
+                        "invalid-tfa-code": "Code TFA invalide, veuillez réessayer.",
+                        "rate-limit-exceeded": "Vous avez trop essayé de désactiver TFA, veuillez réessayer plus tard.",
+                    },
+                    activerTfa: {
+                        title: "Authentification à deux facteurs",
+                        warning:
+                            "ASSUREZ-VOUS D'ENTRER LE CODE SUR VOTRE APPLICATION TFA, VOUS NE VERREZ PLUS CE CODE, SI VOUS LE PERDEZ, VOUS PERDEZ VOTRE COMPTE",
+                        done: "Fait",
+                    },
+                    backupTfa: {
+                        title: "Authentification à deux facteurs",
+                        subTitle: "Pour confirmer et obtenir vos codes de secours, veuillez insérer un code généré par votre application TFA",
+                        warning:
+                            "Ce sont vos codes de sauvegarde, si vous perdez votre appareil, vous pouvez utiliser ces codes pour récupérer votre compte, vous ne les reverrez plus, alors enregistrez-les dans un endroit sûr.",
+
+                        done: "fait",
+                        submit: "Soumettre",
+
+                        "missing-parameters": "Veuillez remplir toutes les entrées.",
+                        "invalid-tfa-code": "Code TFA invalide, veuillez réessayer.",
+                        "rate-limit-exceeded": "Vous avez trop essayé de désactiver TFA, veuillez réessayer plus tard.",
+                    },
+                },
+            },
+            account: {
+                emailStatus: "État de l'e-mail",
+                emailVerified: "E-mail vérifié",
+                emailNotVerified: "E-mail non vérifié",
+
+                logout: "Déconnexion",
+                logoutDesc: "Vos messages seront supprimés",
+
+                deleteAccount: "Supprimer le compte",
+                deleteAccount: "Supprimer tout sur votre compte",
+
+                dialogues: {
+                    verifyEmail: {
+                        title: "Vérifiez votre adresse e-mail",
+                        subTitle: "Vérifiez votre e-mail pour activer TFA et recevez les mises à jour de sécurité directement dans votre e-mail.",
+
+                        verify: "Vérifier",
+                        cancel: "Annuler",
+                    },
+                    verificationEmailSent: {
+                        title: "vérifiez votre adresse e-mail",
+                        subTitle: "Nous avons envoyé un e-mail à & avec un code, il expirera dans 5 heures.",
+                        ok: "OK",
+                    },
+                    logout: {
+                        title: "Déconnexion",
+                        warning: "Tous vos messages seront supprimés pour des raisons de sécurité.",
+                        cancel: "Annuler",
+                    },
+                    deleteAccount: {
+                        title: "Supprimer le compte",
+                        warning: "Vous ne pourrez pas récupérer votre compte après cela, procédez avec prudence.",
+
+                        password: "Mot de passe",
+                        tfaCode: "Code TFA/Code de sauvegarde",
+
+                        cancel: "Annuler",
+
+                        "missing-parameters": "Veuillez remplir toutes les entrées.",
+                        "rate-limit-exceeded": "Vous avez trop essayé de supprimer votre compte, veuillez réessayer plus tard.",
+                        "invalid-tfa-code": "Code TFA invalide",
+                        unauthorized: "Mot de passe invalide",
+                    },
+                },
             },
         },
     },
--- locales/pr.js
+++ locales/pr.js
@@ -4,102 +4,319 @@ const pr = {
             title: "Bem-vindo ao aplicativo!",
 
             navbar: {
-                login: "Conecte-se",
-                register: "Registro",
-                about: "Sobre",
-                support: "Apoiar",
-                download: "Download",
+                login: "Entrar",
+                register: "Registrar",
+                about: "Acerca",
+                support: "Suporte",
+                download: "Baixar",
             },
         },
         home: {
-            title: "Página principal",
+            pageTitle: "DIMLIM | Casa",
+            title: "Casa",
 
             navbar: {
                 profile: "Perfil",
-                settings: "Definições",
+                settings: "Configurações",
 
                 logout: "Sair",
+            },
+
+            contacts: "Contatos",
+            blocked: "Bloqueado",
+
+            addContact: "Adicionar contato",
+            noContacts: "Você ainda não tem nenhum contato, comece pressionando Adicionar contato",
+
+            blockedContacts: "Contatos bloqueados",
+            noBlockedContacts: "Você ainda não tem nenhum contato bloqueado",
+
+            dialogs: {
+                addContact: {
+                    title: "Adicionar contato",
+                    label: "Por favor, insira o nome de usuário da pessoa",
+
+                    cancel: "Cancelar",
+                    add: "Adicionar",
+
+                    "missing-parameters": "Por favor, preencha todas as entradas.",
+                    "self-add": "Você não pode se adicionar como contato",
+                    "already-on-list": "Esse usuário já está em seus contatos.",
+                    "user-not-found": "Não há usuário com esse nome de usuário",
+                },
             },
         },
     },
     accounts: {
         login: {
-            pageTitle: "DIMLIM | Conecte-se",
-            title: "Entrar em DIMLIM",
+            pageTitle: "DIMLIM | Login",
+            title: "Entrar no DIMLIM",
 
-            email: "Email",
+            email: "E-mail",
             password: "Senha",
             tfa: "Autenticação de dois fatores",
 
-            login: "Conecte-se",
-            submit: "Registrarse",
+            login: "Entrar",
+            submit: "Enviar",
             forgotPassword: "Esqueceu sua senha?",
-            doNotHaveAnAccount: "Não tem uma conta ainda? & Registro",
-            tfaHelp: "Acesse seu aplicativo de autenticação e insira o código de 6 dígitos ou insira um de seus códigos de backup",
+            doNotHaveAnAccount: "Ainda não tem uma conta? & Registre-se",
+            tfaHelp: "Vá para seu aplicativo de autenticação e insira o código de 6 dígitos ou insira um de seus códigos de backup",
 
-            emailRequired: "O email é obrigatório.",
-            passwordRequired: "Senha requerida.",
-            emailInvalid: "Email inválido.",
-            emailOrPasswordIncorrect: "Email ou senha está incorreto.",
+            emailRequired: "O e-mail é obrigatório.",
+            passwordRequired: "A senha é obrigatória.",
+            emailInvalid: "E-mail inválido.",
+            emailOrPasswordIncorrect: "E-mail ou senha incorretos.",
             rateLimitExceeded: "Muitas tentativas de login, tente novamente mais tarde.",
-            invalidTfa: "Código TFA inválido. Tente novamente.",
+            invalidTfa: "Código TFA inválido, tente novamente.",
 
             navbar: {
-                login: "Conecte-se",
-                register: "Registro",
-                about: "Sobre",
-                support: "Apoiar",
-                download: "Download",
+                login: "Entrar",
+                register: "Registrar",
+                about: "Acerca",
+                support: "Suporte",
+                download: "Baixar",
             },
         },
         register: {
-            pageTitle: "DIMLIM | Registro",
-            title: "Registre-se no DIMLIM",
+            pageTitle: "DIMLIM | Cadastro",
+            title: "Registrar no DIMLIM",
 
-            email: "Email",
+            email: "E-mail",
             username: "Nome de usuário",
             password: "Senha",
-            confirmPassword: "Confirme a Senha",
+            confirmPassword: "Confirmar Senha",
 
-            register: "Registro",
-            alreadyHaveAnAccount: "já tem uma conta? & Conecte-se",
+            register: "Registrar",
+            alreadyHaveAnAccount: "Já tem uma conta? & Login",
 
-            emailRequired: "O e-mail é obrigatório",
-            usernameRequired: "Nome de usuário é requerido",
-            passwordRequired: "Senha requerida",
+            emailRequired: "Email obrigatório",
+            usernameRequired: "O nome de usuário é obrigatório",
+            passwordRequired: "A senha é obrigatória",
             confirmPasswordRequired: "Por favor, confirme sua senha",
 
-            emailInvalid: "Email inválido",
+            emailInvalid: "E-mail inválido",
             usernameInvalid: "Os nomes de usuário podem conter apenas letras, números e pontos (.)",
             usernameLength: "O nome de usuário deve ter entre 3 e 32 caracteres",
             passwordLength: "A senha deve ter entre 8 e 256 caracteres",
 
             usernameInUse: "O nome de usuário já está em uso",
-            emailInUse: "O email já está sendo usado",
+            emailInUse: "E-mail já está em uso",
             rateLimitExceeded: "Você já registrou uma conta recentemente, tente novamente mais tarde",
 
             navbar: {
-                login: "Conecte-se",
-                register: "Registro",
-                about: "Sobre",
-                support: "Apoiar",
-                download: "Download",
+                login: "Entrar",
+                register: "Registrar",
+                about: "Acerca",
+                support: "Suporte",
+                download: "Baixar",
             },
         },
-    },
-    settings: {
-        index: {
-            pageTitle: "DIMLIM | Definições",
-            title: "DIMLIM Definições",
+        verifyEmail: {
+            pageTitle: "DIMLIM | Verificar e-mail",
+
+            expired: "O código expirou.",
+            invalid: "O código que você digitou é inválido.",
+            successNotLoggedIn: "Seu e-mail foi verificado, agora você pode fazer login em sua conta.",
+            successLoggedIn: "Seu e-mail foi verificado.",
+
+            entrar: "Entrar",
+            goHome: "Ir para casa",
 
             navbar: {
+                login: "Entrar",
+                register: "Registrar",
+                about: "Acerca",
+                support: "Suporte",
+                download: "Baixar",
+
                 profile: "Perfil",
-                settings: "Definições",
+                settings: "Configurações",
 
                 logout: "Sair",
             },
         },
     },
+    settings: {
+        index: {
+            pageTitle: "DIMLIM | Configurações",
+            title: "Configurações DIMLIM",
+
+            navbar: {
+                profile: "Perfil",
+                settings: "Configurações",
+
+                logout: "Sair",
+            },
+
+            tabs: {
+                general: "Geral",
+                security: "Segurança",
+                privacy: "Privazio",
+                account: "Conta",
+            },
+
+            general: {
+                title: "Informações Gerais",
+                username: "Nome de usuário:",
+                bio: "Bio:",
+                email: "E-mail:",
+                preferredLanguage: "Idioma preferido:",
+                contacts: "Contatos:",
+                blockedContacts: "Contatos bloqueados:",
+                criação: "Data de criação da conta:",
+
+                locales: {
+                    de: "Deutsch",
+                    en: "English",
+                    es: "Español",
+                    fr: "Français",
+                    pr: "Português",
+                },
+
+                dialogs: {
+                    avatar: {
+                        title: "Deseja usar esta imagem como seu avatar?",
+                        cancel: "Cancelar",
+                        yes: "Sim",
+                    },
+                    email: {
+                        title: "Por favor, insira seu novo e-mail",
+                        subTitle: "Este e-mail não será verificado e você terá que passar pelo processo de re-verificação de seu e-mail",
+
+                        newEmail: "Seu novo e-mail",
+                        password: "Sua senha atual",
+
+                        cancel: "Cancelar",
+                        yes: "Sim",
+
+                        missing: "Por favor, preencha todas as entradas",
+                        invalidEmail: "O e-mail que você digitou é inválido",
+
+                        "email-already-in-use": "O e-mail que você digitou já está em uso",
+                        "user-not-found": "Certo amigo como diabos você está fazendo isso então, eu nem vou me incomodar",
+                        "rate-limit-exceeded": "Você tentou alterar seu e-mail muitas vezes, tente novamente mais tarde.",
+                    },
+                    language: {
+                        title: "Por favor, selecione um idioma",
+                        subTitle: "Isso definirá o idioma do aplicativo",
+
+                        cancel: "Cancelar",
+                        change: "Alterar idioma",
+                    },
+                    bio: {
+                        title: "Por favor, insira sua nova biografia",
+                        cancel: "Cancelar",
+                        change: "Alterar biografia",
+                    },
+                },
+            },
+            segurança: {
+                password: "Senha",
+                tfa: "Autenticação de dois fatores",
+
+                tfaActive: "Ativo",
+                tfaNotActive: "Não configurado",
+
+                dialogs: {
+                    password: {
+                        title: "Alterar senha",
+                        old: "Sua senha antiga",
+                        new: "Sua nova senha",
+                        tfa: "Código TFA/Código de Backup",
+
+                        cancel: "Cancelar",
+                        change: "Alterar senha",
+
+                        unauthorized: "Senha inválida.",
+                        "missing-parameters": "Por favor, preencha todas as entradas.",
+                        "invalid-parameters": "Sua nova senha deve ter mais de 8 caracteres e menos de 256 caracteres.",
+                        "invalid-tfa-code": "Código TFA inválido, tente novamente.",
+                        "rate-limit-exceeded": "Você tentou alterar sua senha muitas vezes, tente novamente mais tarde.",
+                    },
+                    setupTfa: {
+                        title: "Autenticação de dois fatores",
+                        subTitle: "TFA adiciona uma camada extra de segurança na qual você também precisa de um dispositivo para fazer login em sua conta.",
+                        deactivateLabel: "Código TFA/Código de Backup",
+
+                        emailVerified: "Você precisa verificar seu e-mail para habilitar o TFA",
+
+                        activate: "Ativar",
+                        deactivate: "Desativar",
+                        cancel: "Cancelar",
+                        ok: "Ok",
+
+                        "missing-parameters": "Por favor, preencha todas as entradas.",
+                        "invalid-tfa-code": "Código TFA inválido, tente novamente.",
+                        "rate-limit-exceeded": "Você tentou desativar o TFA muitas vezes, tente novamente mais tarde.",
+                    },
+                    activeTfa: {
+                        title: "Autenticação de dois fatores",
+                        warning:
+                            "CERTIFIQUE-SE DE INSERIR O CÓDIGO NO SEU APLICATIVO TFA, VOCÊ NÃO VERÁ ESTE CÓDIGO NOVAMENTE, SE VOCÊ PERDER, PERDE SUA CONTA",
+                        done: "Feito",
+                    },
+                    backupTfa: {
+                        title: "Autenticação de dois fatores",
+                        subTitle: "Para confirmar e obter seus códigos de backup, insira um código gerado pelo seu aplicativo TFA",
+                        warning:
+                            "Estes são seus códigos de backup, se você perder seu dispositivo, você pode usar esses códigos para recuperar sua conta, você não os verá novamente, então salve-os em algum lugar seguro.",
+
+                        done: "Feito",
+                        enviar: "Enviar",
+
+                        "missing-parameters": "Por favor, preencha todas as entradas.",
+                        "invalid-tfa-code": "Código TFA inválido, tente novamente.",
+                        "rate-limit-exceeded": "Você tentou desativar o TFA muitas vezes, tente novamente mais tarde.",
+                    },
+                },
+            },
+            account: {
+                emailStatus: "Status do e-mail",
+                emailVerified: "Email verificado",
+                emailNotVerified: "E-mail não verificado",
+
+                logout: "Sair",
+                logoutDesc: "Suas mensagens serão deletadas",
+
+                deleteAccount: "Excluir conta",
+                deleteAccount: "Excluir tudo em sua conta",
+
+                dialogs: {
+                    verifyEmail: {
+                        title: "Verifique seu e-mail",
+                        subTitle: "Verifique seu e-mail para ativar o TFA e receber atualizações de segurança diretamente em seu e-mail.",
+
+                        verify: "Verificar",
+                        cancel: "Cancelar",
+                    },
+                    verificationEmailSent: {
+                        title: "verifique seu e-mail",
+                        subTitle: "Enviamos um e-mail para & com um código, ele expira em 5 horas.",
+                        ok: "Ok",
+                    },
+                    logout: {
+                        title: "Sair",
+                        warning: "Todas as suas mensagens serão deletadas por motivos de segurança.",
+                        cancel: "Cancelar",
+                    },
+                    deleteAccount: {
+                        title: "Excluir conta",
+                        warning: "Você não poderá recuperar sua conta depois disso, prossiga com cuidado.",
+
+                        password: "Senha",
+                        tfaCode: "Código TFA/Código de Backup",
+
+                        cancel: "Cancelar",
+
+                        "missing-parameters": "Por favor, preencha todas as entradas.",
+                        "rate-limit-exceeded": "Você tentou excluir sua conta muitas vezes, tente novamente mais tarde.",
+                        "invalid-tfa-code": "Código TFA inválido",
+                        unauthorized: "Senha inválida",
+                    },
+                },
+            },
+        },
+    },
 };
 
-module.exports = pr;
+modulo.exports = pr;

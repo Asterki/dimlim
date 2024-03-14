621064ae15f25d4c3ab093833096ef5500d5d784
Asterki <asterki.dev@proton.me>
2024-03-13 20:36:14 -0600

    Feat: Basic login page

--- client/src/pages/accounts/login.tsx
+++ client/src/pages/accounts/login.tsx
@@ -1,9 +1,30 @@
+import NavbarComponent from "../../components/navbar";
+
 const AccountLogin = () => {
     return (
-        <div>
-            <p>ejqwioejqwoij</p>
+        <div className="bg-gray-800 min-h-screen text-white">
+            <NavbarComponent user={null} />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-700 rounded-md p-4 w-4/12">
+                <form>
+                    <h1 className="text-2xl text-center">Login to DIMLIM</h1>
+
+                    <div className="my-2">
+                        <label>Email</label>
+                        <input type="email" className="w-full p-2 bg-gray-800 border-gray-600 outline-none rounded-md" />
+                    </div>
+
+                    <div className="my-2">
+                        <label>Password</label>
+                        <input type="password" className="w-full p-2 bg-gray-800 border-gray-600 outline-none rounded-md" />
+                    </div>
+
+                    <div className="mt-4">
+                        <button className="w-full p-2 bg-blue-500 rounded-md">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
-}
- 
+};
+
 export default AccountLogin;

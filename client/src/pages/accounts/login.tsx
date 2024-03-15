0ba9b928f307a1ee26c179c9566cb9bc8e1baf9e
Asterki <asterki.dev@proton.me>
2024-03-14 18:42:55 -0600

    Feat: Updated design of login page

--- client/src/pages/accounts/login.tsx
+++ client/src/pages/accounts/login.tsx
@@ -4,23 +4,25 @@ const AccountLogin = () => {
     return (
         <div className="bg-gray-800 min-h-screen text-white">
             <NavbarComponent user={null} />
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-700 rounded-md p-4 w-4/12">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-700 rounded-md p-4 w-11/12 md:w-4/12">
                 <form>
-                    <h1 className="text-2xl text-center">Login to DIMLIM</h1>
+                    <h1 className="text-2xl font-semibold mb-2">Login to DIMLIM</h1>
 
-                    <div className="my-2">
+                    <div className="my-4">
                         <label>Email</label>
-                        <input type="email" className="w-full p-2 bg-gray-800 border-2 border-gray-800 outline-none rounded-md transition-all focus:border-blue-400" />
+                        <input type="email" placeholder="email@example.com" className="w-full p-2 bg-gray-800 border-2 border-gray-800 outline-none rounded-md transition-all focus:border-blue-400" />
                     </div>
 
-                    <div className="my-2">
+                    <div className="my-4">
                         <label>Password</label>
-                        <input type="password" className="w-full p-2 bg-gray-800 border-2 border-gray-800 outline-none rounded-md transition-all focus:border-blue-400" />
+                        <input type="password" placeholder="••••••••" className="w-full p-2 bg-gray-800 border-2 border-gray-800 outline-none rounded-md transition-all focus:border-blue-400" />
                     </div>
 
-                    <div className="mt-4">
+                    <div className="mt-8">
                         <button className="w-full p-2 bg-blue-500 rounded-md">Login</button>
                     </div>
+
+                    <div className="mt-4">Don't have an account yet? <a href="/register" className="text-blue-400">Register</a></div>
                 </form>
             </div>
         </div>

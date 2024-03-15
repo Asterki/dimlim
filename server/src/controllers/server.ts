5d6ef57bb65cc0ca9c18e6ea9ce20b4b8b892df7
Asterki <asterki.dev@proton.me>
2024-03-14 20:49:44 -0600

    Feat: Created Session Controller

--- server/src/controllers/server.ts
+++ server/src/controllers/server.ts
@@ -3,32 +3,25 @@ import { Authenticator } from "@fastify/passport";
 
 // Middleware
 import cors from "@fastify/cors";
-import fastifyCookie from "@fastify/cookie";
-import fastifySession from "@fastify/session";
 
 import Router from "../routes/router";
-
-
-import authenticationStrategies from "./sessions";
+import SessionController from "./sessions";
 
 class Server {
     fastify: FastifyInstance;
-    fastifyPassport: Authenticator;
+    fastifyPassport!: Authenticator;
     port: number;
 
     constructor(dev: boolean, port: number) {
         this.fastify = Fastify({
-            logger: true,
+            logger: dev,
         });
         this.port = port;
-        this.fastifyPassport = new Authenticator();
     }
 
     public getServer() {
         return this.fastify;
     }
-
-    public addSessionController() {}
 
     async start() {
         this.loadMiddlewares();
@@ -45,16 +38,10 @@ class Server {
     }
 
     private loadSessionController() {
-        this.fastify.register(fastifyCookie);
-        this.fastify.register(fastifySession, {
-            secret: "secret with minimum length of 32 characters",
-        });
-
-        this.fastify.register(this.fastifyPassport.initialize());
-        this.fastify.register(this.fastifyPassport.secureSession());
-
-        // Registering strategies
-        this.fastifyPassport.use("local", authenticationStrategies.local); 
+        const sessionController = new SessionController();
+        this.fastifyPassport = sessionController.getPassport();
+        sessionController.addStrategies();
+        sessionController.loadMiddleware(this.fastify);
     }
 
     private registerRoutes() {
--- server/src/controllers/sessions.ts
+++ server/src/controllers/sessions.ts
@@ -1,14 +1,50 @@
-import { Strategy as LocalStrategy } from 'passport-local';
+import { FastifyInstance } from "fastify";
+import { Authenticator } from "@fastify/passport";
 
-const authenticationStrategies = {
-    local: new LocalStrategy((username: string, password: string, done) => {
-         //   User.findOne({ username: username }, function (err, user) {
-        //     if (err) { return done(err); }
-        //     if (!user) { return done(null, false); }
-        //     if (!user.verifyPassword(password)) { return done(null, false); }
-        //     return done(null, user);
-        //   });
-    })
+import fastifyCookie from "@fastify/cookie";
+import fastifySession from "@fastify/session";
+
+import { Strategy as LocalStrategy } from "passport-local";
+
+class SessionController {
+    authenticationStrategies: {
+        local: LocalStrategy;
+    };
+    fastifyPassport: Authenticator;
+
+    constructor() {
+        this.fastifyPassport = new Authenticator();
+        this.authenticationStrategies = {
+            local: new LocalStrategy(
+                (username: string, password: string, done) => {
+                    //   User.findOne({ username: username }, function (err, user) {
+                    //     if (err) { return done(err); }
+                    //     if (!user) { return done(null, false); }
+                    //     if (!user.verifyPassword(password)) { return done(null, false); }
+                    //     return done(null, user);
+                    //   });
+                }
+            ),
+        };
+    }
+
+    public getPassport() {
+        return this.fastifyPassport;
+    }
+
+    public addStrategies() {
+        this.fastifyPassport.use("local", this.authenticationStrategies.local);
+    }
+
+    public loadMiddleware(server: FastifyInstance) {
+        server.register(fastifyCookie);
+        server.register(fastifySession, {
+            secret: "secret with minimum length of 32 characters",
+        });
+
+        server.register(this.fastifyPassport.initialize());
+        server.register(this.fastifyPassport.secureSession());
+    }
 }
 
-export default authenticationStrategies;
+export default SessionController;

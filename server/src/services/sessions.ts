c1df745907ebc6218e0137854585d2a05b01f041
Asterki <asterki.dev@proton.me>
2024-03-15 17:44:24 -0600

    Feat: Local Strategy

--- server/src/services/sessions.ts
+++ server/src/services/sessions.ts
@@ -6,6 +6,8 @@ import fastifySession from "@fastify/session";
 
 import { Strategy as LocalStrategy } from "passport-local";
 
+import UserModel from "../models/users";
+
 class SessionController {
     authenticationStrategies: {
         local: LocalStrategy;
@@ -16,8 +18,38 @@ class SessionController {
         this.fastifyPassport = new Authenticator();
         this.authenticationStrategies = {
             local: new LocalStrategy(
-                (username: string, password: string, done) => {
-                    
+                {
+                    usernameField: "email",
+                    passwordField: "password",
+                    passReqToCallback: true,
+                    session: true,
+                },
+                async (req: express.Request, _email: string, _password: string, done) => {
+                    try {
+                        const user: (User & Document) | null = await UserModel.findOne({
+                            $or: [{ "email.value": req.body.email }, { username: req.body.email }],
+                        });
+                        if (!user) return done(null, false, { message: "invalid-credentials" });
+
+                        // Verify password and TFA code
+                        if (!bcrypt.compareSync(req.body.password, user.password))
+                            return done(null, false, { message: "invalid-credentials" });
+                        if (user.tfa.secret !== "") {
+                            if (!req.body.tfaCode) return done(null, false, { message: "requires-tfa" });
+
+                            const verified = speakeasy.totp.verify({
+                                secret: user.tfa.secret,
+                                encoding: "base32",
+                                token: req.body.tfaCode,
+                            });
+
+                            if (verified == false) return done(null, false, { message: "invalid-tfa-code" });
+                        }
+
+                        return done(null, user);
+                    } catch (err: unknown) {
+                        return done(err);
+                    }
                 }
             ),
         };

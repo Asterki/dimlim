86664d07f540cb087c8fea5c26f1f9227a15f200
asterki <asterki.dev@proton.me>
2024-04-03 18:00:29 -0600

    Feat: Switched to primitives navbar dropdown

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 import { User } from "../../../shared/types/models";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
+
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 interface ComponentProps {
     user: User | null;
@@ -18,65 +19,63 @@ const NavbarComponent: React.FC<ComponentProps> = (props) => {
         <div className="w-full flex items-center justify-between p-4 h-16 bg-gray-700 shadow-md z-10 absolute top-0">
             <h1 className="text-2xl text-white font-bold">DIMLIM</h1>
             {props.user && (
-                <div className="w-2/12 relative">
-                    <div
-                        onClick={() => setMenuOpen(!menuOpen)}
-                        className="flex items-center justify-between px-4 py-2 rounded-md border-2 border-white/20 transition-all hover:bg-white/10 cursor-pointer w-full"
+                <div className="w-2/12">
+                    <DropdownMenu.Root
+                        open={menuOpen}
+                        onOpenChange={(change) => {
+                            setMenuOpen(change);
+                        }}
+                        
                     >
-                        <div className="flex items-center justify-center">
+                        <DropdownMenu.Trigger className="outline-none flex items-center justify-between px-4 py-2 rounded-md border-2 border-white/20 transition-all hover:bg-white/10 cursor-pointer w-full">
+                            <div className="flex items-center justify-center">
+                                <div>
+                                    <img
+                                        width={30}
+                                        src="https://www.asterki.com/assets/images/icon.png"
+                                        alt="wjaoi"
+                                        className="mr-2 rounded-full"
+                                    />
+                                </div>
+                                <div>{props.user.profile.username}</div>
+                            </div>
                             <div>
-                                <img
-                                    width={30}
-                                    src="https://www.asterki.com/assets/images/icon.png"
-                                    alt="wjaoi"
-                                    className="mr-2 rounded-full"
+                                <FontAwesomeIcon
+                                    className={`${
+                                        menuOpen ? "transform rotate-180" : ""
+                                    } transition-all`}
+                                    icon={faChevronCircleDown}
                                 />
                             </div>
-                            <div>{props.user.profile.username}</div>
-                        </div>
-                        <div>
-                            <FontAwesomeIcon
-                                className="text-white/80"
-                                icon={faChevronCircleDown}
-                            />
-                        </div>
-                    </div>
+                        </DropdownMenu.Trigger>
 
-                    <motion.div
-                        variants={{
-                            open: {
-                                y: "auto",
-                                display: "block",
-                                opacity: 1,
-                            },
-                            closed: {
-                                y: 0,
-                                opacity: 0,
-                                transitionEnd: {
-                                    display: "none",
-                                },
-                            },
-                        }}
-                        initial="closed"
-                        animate={menuOpen ? "open" : "closed"}
-                        className="w-full absolute bg-gray-700 rounded-md border-2 border-white/20 mt-2 p-2 "
-                    >
-                        <Link to="/contacts">
-                            <div className="p-2 transitiona-all hover:bg-white/20 rounded-md cursor-pointer w-full">
-                                Settings
-                            </div>
-                        </Link>
-                        <Link to="/contacts">
-                            <div className="p-2 transitiona-all hover:bg-white/20 rounded-md cursor-pointer w-full">
-                                Contacts
-                            </div>
-                        </Link>
-                        <Link to="/contacts">
-                            <div className="p-2 transitiona-all hover:bg-white/20 rounded-md cursor-pointer w-full">
-                                Logout
-                            </div>
-                        </Link>
-                    </motion.div>
+                        <DropdownMenu.Portal>
+                            <DropdownMenu.Content align="end" className="text-white bg-gray-700 transition-all rounded-md border-2 border-white/20 w-12/12">
+                                <DropdownMenu.Item className="p-2 transition-all hover:bg-white/20 cursor-pointer w-full">
+                                    <Link to="/settings">Settings</Link>
+                                </DropdownMenu.Item>
+                                <DropdownMenu.Item className="p-2 transition-all hover:bg-white/20 cursor-pointer w-full">
+                                    <Link to="/contacts">Contacts</Link>
+                                </DropdownMenu.Item>
+
+                                <DropdownMenu.Item className="p-2 transition-all hover:bg-white/20 cursor-pointer w-full">
+                                    <Link to="/contacts">Logout</Link>
+                                </DropdownMenu.Item>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Portal>
+                    </DropdownMenu.Root>
+
+                    {/* 
+                    <Link to="/contacts">
+                        <div>
+                            Contacts
+                        </div>
+                    </Link>
+                    <Link to="/contacts">
+                        <div>
+                            Logout
+                        </div>
+                    </Link> */}
                 </div>
             )}
             {!props.user && (

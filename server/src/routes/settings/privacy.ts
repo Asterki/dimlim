8fc42062002afc7c4a87d3e7783fca6d05926bf4
asterki <asterki.dev@proton.me>
2024-04-13 09:48:13 -0600

    Feat: Privacy settings API route

---
+++ server/src/routes/settings/privacy.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+import UserModel from "../../models/users";
+
+import { AddResponseData as ResponseData } from "../../../../shared/types/api/contacts";
+import { NextFunction, Request, Response } from "express";
+import { User } from "../../../../shared/types/models";
+
+// Settings Privact
+const handler = async (req: Request, res: Response<ResponseData>, next: NextFunction) => {
+    if (req.isUnauthenticated() || !req.user) return res.status(401).send({ status: "unauthenticated" });
+    const currentUser = req.user as User;
+
+    const parsedBody = z
+        .object({
+            showOnlineStatus: z.boolean(),
+            showLastSeen: z.boolean(),
+            showReadReceipts: z.boolean(),
+        })
+        .safeParse(req.body);
+
+    if (!parsedBody.success)
+        return res.status(400).send({
+            status: "invalid-parameters",
+        });
+   
+    
+    await UserModel.updateOne(
+        { userID: currentUser.userID },
+        {
+            $set: {
+                "preferences.privacy.showOnlineStatus": parsedBody.data.showOnlineStatus,
+                "preferences.privacy.showLastSeen": parsedBody.data.showLastSeen,
+                "preferences.privacy.showReadReceipts": parsedBody.data.showReadReceipts,
+            },
+        }
+    ).exec();
+
+
+    return res.status(200).send({
+        status: "success",
+    });
+};
+
+export default handler;

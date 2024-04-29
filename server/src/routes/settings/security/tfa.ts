aa59bfdeef62be210387b39a64f92c9aabbf3dae
asterki <asterki.dev@proton.me>
2024-04-29 17:05:12 -0600

    Feat: Moved TFA operations into one file

--- server/src/routes/settings/security/tfa.ts
+++ server/src/routes/settings/security/tfa.ts
@@ -9,7 +9,7 @@ import { User } from "../../../../../shared/types/models";
 
 import Logger from "../../../services/logger";
 
-// Activate TFA
+// Activate/Deactivate TFA
 const handler = async (req: Request, res: Response<ResponseData>, next: NextFunction) => {
     if (req.isUnauthenticated() || !req.user) return res.status(401).send({ status: "unauthenticated" });
     const currentUser = req.user as User;
@@ -17,7 +17,8 @@ const handler = async (req: Request, res: Response<ResponseData>, next: NextFunc
     const parsedBody = z
         .object({
             password: z.string(),
-            secret: z.string(),
+            action: z.enum(["activate", "deactivate"]),
+            secret: z.string().optional(),
         })
         .safeParse(req.body);
 
@@ -30,21 +31,30 @@ const handler = async (req: Request, res: Response<ResponseData>, next: NextFunc
         const user = await UserModel.findOne({ userID: currentUser.userID }).exec();
         if (!user) return res.status(404).send({ status: "not-found" });
 
-        const validPassword = await bcrypt.compare(
-            parsedBody.data.password,
-            currentUser.preferences.security.password
-        );
+        const validPassword = await bcrypt.compare(parsedBody.data.password, currentUser.preferences.security.password);
         if (!validPassword) return res.status(401).send({ status: "unauthenticated" });
 
-        UserModel.updateOne(
-            { userID: currentUser.userID },
-            {
-                $set: {
-                    "preferences.security.twoFactor.active": true,
-                    "preferences.security.twoFactor.secret": parsedBody.data.secret,
-                },
-            }
-        ).exec();
+        if (parsedBody.data.action === "activate") {
+            UserModel.updateOne(
+                { userID: currentUser.userID },
+                {
+                    $set: {
+                        "preferences.security.twoFactor.active": true,
+                        "preferences.security.twoFactor.secret": parsedBody.data.secret,
+                    },
+                }
+            ).exec();
+        } else {
+            UserModel.updateOne(
+                { userID: currentUser.userID },
+                {
+                    $set: {
+                        "preferences.security.twoFactor.active": false,
+                        "preferences.security.twoFactor.secret": "",
+                    },
+                }
+            ).exec();
+        }
 
         return res.status(200).send({
             status: "success",

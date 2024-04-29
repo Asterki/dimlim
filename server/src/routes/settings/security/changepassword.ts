257f91cf2c3fcd0a70a38502da281f38c6d2c785
asterki <asterki.dev@proton.me>
2024-04-29 17:03:55 -0600

    Refactor: Moved API routes to different files

---
+++ server/src/routes/settings/security/changepassword.ts
@@ -0,0 +1,61 @@
+import { z } from "zod";
+import bcrypt from "bcrypt";
+
+import UserModel from "../../../models/users";
+
+import { SecurityResponseData as ResponseData } from "../../../../../shared/types/api/settings";
+import { NextFunction, Request, Response } from "express";
+import { User } from "../../../../../shared/types/models";
+
+import Logger from "../../../services/logger";
+
+// Activate TFA
+const handler = async (req: Request, res: Response<ResponseData>, next: NextFunction) => {
+    if (req.isUnauthenticated() || !req.user) return res.status(401).send({ status: "unauthenticated" });
+    const currentUser = req.user as User;
+
+    const parsedBody = z
+        .object({
+            newPassword: z.string(),
+            oldPassword: z.string(),
+        })
+        .safeParse(req.body);
+
+    if (!parsedBody.success)
+        return res.status(400).send({
+            status: "invalid-parameters",
+        });
+
+    try {
+        const user = await UserModel.findOne({ userID: currentUser.userID }).exec();
+        if (!user) return res.status(404).send({ status: "not-found" });
+
+        const validPassword = await bcrypt.compare(
+            parsedBody.data.oldPassword,
+            currentUser.preferences.security.password
+        );
+        if (!validPassword) return res.status(401).send({ status: "unauthenticated" });
+
+        const pass = await bcrypt.hash(parsedBody.data.newPassword, 10);
+
+        UserModel.updateOne(
+            { userID: currentUser.userID },
+            {
+                $set: {
+                    "preferences.security.password": pass,
+                },
+            }
+        ).exec();
+
+        return res.status(200).send({
+            status: "success",
+        });
+    } catch (error: unknown) {
+        res.status(500).send({
+            status: "internal-error",
+        });
+        Logger.getInstance().error((error as Error).message, true);
+    }
+};
+
+export default handler;

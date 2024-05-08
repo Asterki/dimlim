aab8245e0ff34eba5150e85b4936877e684e6208
asterki <asterki.dev@proton.me>
2024-05-07 18:02:56 -0600

    Refactor: profile picture API route

--- server/src/routes/profile/picture.ts
+++ server/src/routes/profile/picture.ts
@@ -1,4 +1,5 @@
 import formidable, { IncomingForm } from 'formidable';
+import { z } from 'zod';
 import path from 'path';
 import sharp from 'sharp';
 import { v4 as uuidv4 } from 'uuid';
@@ -18,70 +19,108 @@ const handler = async (req: Request, res: Response, next: NextFunction) => {
   if (req.isUnauthenticated() || !req.user) return res.status(401).send({ status: 'unauthenticated' });
   const currentUser = req.user as User;
 
+  const parsedBody = z
+    .object({
+      action: z.enum(['upload', 'remove']),
+    })
+    .safeParse(req.body);
+
+  if (!parsedBody.success)
+    return res.status(400).send({
+      status: 'invalid-parameters',
+    });
+
   try {
-    // Read the file
-    const data: { files: formidable.Files; fields: formidable.Fields } = await new Promise((resolve, reject) => {
-      const form = new IncomingForm();
-      form.parse(req, (err, fields, files) => {
-        // TODO: Add filter to only allow images
-        if (err) return reject(err);
+    if (parsedBody.data.action === 'upload') {
+      // Read the file
+      const data: { files: formidable.Files; fields: formidable.Fields } = await new Promise((resolve, reject) => {
+        const form = new IncomingForm();
+        form.parse(req, (err, fields, files) => {
+          // TODO: Add filter to only allow images
+          if (err) return reject(err);
 
-        resolve({ fields, files });
-      });
-    });
-
-    if (!data.files.profile) return res.status(400).json({ message: 'bad-request' });
-    let file = data.files.profile[0];
-
-    // Create the directory if it doesn't exist
-    await UploadService.getInstance().createDirectory(
-      path.join(__dirname, '/public/data/profile-pictures/', currentUser.userID),
-    );
-
-    let imgID = uuidv4();
-
-    // Save the image
-    let newPath =
-      path.join(process.cwd(), '/public/data/profile-pictures/', currentUser!.userID) + '/' + imgID + '.' + 'png';
-    let rawData = fs.readFileSync(file.filepath);
-
-    // Compress the file
-    await sharp(rawData)
-      .resize(256, 256)
-      .png()
-      .toBuffer()
-      .then((data) => {
-        rawData = data;
+          resolve({ fields, files });
+        });
       });
 
-    fs.writeFile(newPath, rawData, function (err) {
-      if (err) console.log(err);
-    });
+      if (!data.files.profile) return res.status(400).json({ message: 'bad-request' });
+      let file = data.files.profile[0];
 
-    // Delete the old user profile picture
-    const userProfile = await UserModel.findOne({
-      userID: currentUser.userID,
-    });
-    if (userProfile && userProfile.profile!.imageID) {
-      fs.unlinkSync(
-        path.join(
-          process.cwd(),
-          '/public/data/profile-pictures/',
-          currentUser.userID,
-          '/' + currentUser.profile.imageID + '.png',
-        ),
+      // Create the directory if it doesn't exist
+      await UploadService.getInstance().createDirectory(
+        path.join(__dirname, '/public/data/profile-pictures/', currentUser.userID),
+      );
+
+      let imgID = uuidv4();
+
+      // Save the image
+      let newPath =
+        path.join(process.cwd(), '/public/data/profile-pictures/', currentUser!.userID) + '/' + imgID + '.' + 'png';
+      let rawData = fs.readFileSync(file.filepath);
+
+      // Compress the file
+      await sharp(rawData)
+        .resize(256, 256)
+        .png()
+        .toBuffer()
+        .then((data) => {
+          rawData = data;
+        });
+
+      fs.writeFile(newPath, rawData, function (err) {
+        if (err) console.log(err);
+      });
+
+      // Delete the old user profile picture
+      const userProfile = await UserModel.findOne({
+        userID: currentUser.userID,
+      });
+      if (userProfile && userProfile.profile!.imageID) {
+        fs.unlinkSync(
+          path.join(
+            process.cwd(),
+            '/public/data/profile-pictures/',
+            currentUser.userID,
+            '/' + currentUser.profile.imageID + '.png',
+          ),
+        );
+      }
+
+      // Update the user's profile picture
+      await UserModel.updateOne(
+        { userID: currentUser.userID },
+        {
+          $set: {
+            'profile.imageID': imgID,
+          },
+        },
+      );
+    } else if (parsedBody.data.action === 'remove') {
+      // Delete the user's profile picture
+      const userProfile = await UserModel.findOne({
+        userID: currentUser.userID,
+      });
+      if (userProfile && userProfile.profile!.imageID) {
+        fs.unlinkSync(
+          path.join(
+            process.cwd(),
+            '/public/data/profile-pictures/',
+            currentUser.userID,
+            '/' + currentUser.profile.imageID + '.png',
+          ),
+        );
+      }
+
+      // Update the user's profile picture
+      await UserModel.updateOne(
+        { userID: currentUser.userID },
+        {
+          $set: {
+            'profile.imageID': "",
+          },
+        },
       );
     }
-
-    // Update the user's profile picture
-    await UserModel.updateOne(
-      { userID: currentUser.userID },
-      {
-        $set: {
-          'profile.imageID': imgID,
-        },
-      },
-    );
 
     return res.status(200).send({
       status: 'success',

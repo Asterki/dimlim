80f589581e3191ed3e909a598919cfd6e978b1f9
Asterki <asterki.dev@proton.me>
2024-11-08 17:45:52 -0600

    Chore: Added types for generate key

---
+++ shared/types/api/crypto.d.ts
@@ -0,0 +1,9 @@
+interface GenerateKeysResponseData {
+  status: 'success' | 'server-error' | 'unauthenticated';
+  data?: {
+    privateKey: string;
+    publicKey: string;
+  };
+}
+
+export type { GenerateKeysResponseData };

2599f1512272867e2143142e184f95db60ab14cb
Asterki <asterki.dev@proton.me>
2024-11-28 22:28:25 -0600

    Refactor: Remove unused key generation controller and routes

--- server/src/routes/crypto.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import GenerateKeys from '../controllers/crypto/generateKeys';
-
-const router = express.Router();
-
-router.get('/generateKeys', GenerateKeys);
-
-export default router;
